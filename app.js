76005f64114747ab92189cc6f53833a3514b35ca
Elliott Thompson <ellth92@gmail.com>
2015-08-17 01:06:27 +0100

    created the backend infastructure to add FeedItems to the database and extract a list of feed items / extract a single feed item

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+require('./models/FeedItem');
+mongoose.connect('mongodb://localhost/lettl-io');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var FeedItem = mongoose.model('FeedItem');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/feeditems', function(req, res, next) {
+  FeedItem.find(function(err, item){
+    if(err){ return next(err); }
+    res.json(item);
+  });
+});
+
+router.post('/feeditem', function(req, res, next) {
+  var feed = new FeedItem(req.body);
+
+  feed.save(function(err, feed){
+    if(err) { return next(err); }
+
+    res.json(feed);
+  });
+});
+
+router.param('feeditem', function(req, res, next, id) {
+  var query = FeedItem.findById(id);
+
+  query.exec(function(e, f) {
+    if (e) { return next(e); }
+    if (!f) { return next(new Error('Can\'t find feeditem')); }
+
+    req.feeditem = f;
+    return next();
+  });
+});
+
+router.get('/feeditem/:feeditem', function(req, res) {
+  res.json(req.feeditem);
 });
 
 module.exports = router;
